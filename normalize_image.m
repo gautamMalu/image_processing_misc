f39f252260ca48dd72363fde5a9467265dfa655c
HoffmanLab <hoffmanlab@hoffmanlab-workstation.(none)>
2014-02-05 16:12:24 -0500

    adding normalize image to repo

---
+++ normalize_image.m
@@ -0,0 +1,35 @@
+function image = normalize_image(image,varargin)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+i_p = inputParser;
+
+i_p.addRequired('image',@isnumeric);
+
+i_p.addParamValue('quantiles',@isnumeric);
+i_p.addParamValue('limits',@isnumeric);
+
+i_p.addParamValue('debug',0,@(x)(isnumeric(x) && (x == 0 || x == 1) || islogical(x)));
+
+i_p.parse(image,varargin{:});
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+image = double(image);
+
+if (not(any(strcmp(i_p.UsingDefaults,'quantiles'))))
+    limits = quantile(image(:),i_p.Results.quantiles);
+elseif (not(any(strcmp(i_p.UsingDefaults,'limits'))))
+    limits = i_p.Results.limits;
+else
+    limits = [min(image(:)),max(image(:))];
+end
+
+image = (image - limits(1))/range(limits);
+
+image(image > 1) = 1;
+image(image < 0) = 0;
