32444d43d82a2f5413555976ad9ef5c25300b447
Matthew Berginski <matthew.berginski@gmail.com>
2014-02-11 22:17:47 -0500

    adding connectivity parameters

--- bwpropclose.m
+++ bwpropclose.m
@@ -1,6 +1,23 @@
-function filtered_image = bwpropclose(bw_image,prop_name,max_val)
+function filtered_image = bwpropclose(bw_image,prop_name,max_val,varargin)
 
-bw_label = bwlabel(bw_image);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+i_p = inputParser;
+i_p.addRequired('bw_image',@(x)isnumeric(x) || islogical(x));
+i_p.addRequired('prop_name',@ischar);
+i_p.addRequired('max_val',@isnumeric);
+
+i_p.addParamValue('connectivity',8,@(x)x == 4 || x == 8);
+
+i_p.parse(bw_image,prop_name,max_val,varargin{:});
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bw_label = bwlabel(bw_image,i_p.Results.connectivity);
 props = regionprops(bw_label,prop_name);
 
 filtered_image = ismember(bw_label,find([props.(prop_name)] <= max_val));
