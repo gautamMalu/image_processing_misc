4fb210ba1f718e2abba6d17a96d8a31657780a65
Matthew Berginski <matthew.berginski@gmail.com>
2014-05-08 13:59:53 -0400

    adding option to return filtered IDs

--- labelpropclose.m
+++ labelpropclose.m
@@ -1,4 +1,4 @@
-function label_filtered = labelpropclose(label_image,prop_name,max_val)
+function [label_filtered,varargout] = labelpropclose(label_image,prop_name,max_val,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Setup variables and parse command line
@@ -9,7 +9,9 @@ i_p.addRequired('label_image',@(x)isnumeric(x) || islogical(x));
 i_p.addRequired('prop_name',@ischar);
 i_p.addRequired('max_val',@isnumeric);
 
-i_p.parse(label_image,prop_name,max_val);
+i_p.addOptional('return_filtered_ids',0);
+
+i_p.parse(label_image,prop_name,max_val,varargin{:});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main
@@ -17,6 +19,13 @@ i_p.parse(label_image,prop_name,max_val);
 label_image = double(label_image);
 
 props = regionprops(label_image,prop_name);
+
+if (i_p.Results.return_filtered_ids)
+    id_nums = nonzeros(unique(label_image(:)));
+    passed_ids = find([props.(prop_name)] <= max_val);
+    not_passed_ids = setdiff(id_nums,passed_ids);
+    varargout{1} = not_passed_ids;
+end
 
 filtered_image = ismember(label_image,find([props.(prop_name)] <= max_val));
 
