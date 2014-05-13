720258c8625ce3fb6bced1d59b5753412f84c5ae
Matthew Berginski <matthew.berginski@gmail.com>
2014-05-13 13:40:29 -0400

    adding auto folder creation, data var name

--- csvwrite_with_headers/csvwrite_with_headers.m
+++ csvwrite_with_headers/csvwrite_with_headers.m
@@ -4,21 +4,21 @@
 % known limitations
 % 	The same limitation that apply to the data structure that exist with 
 %   csvwrite apply in this function, notably:
-%       m must not be a cell array
+%       data must not be a cell array
 %
 % Inputs
 %   
 %   filename    - Output filename
-%   m           - array of data
+%   data        - array of data
 %   headers     - a cell array of strings containing the column headers. 
-%                 The length must be the same as the number of columns in m.
+%                 The length must be the same as the number of columns in data.
 %   r           - row offset of the data (optional parameter)
 %   c           - column offset of the data (optional parameter)
 %
 %
 % Outputs
 %   None
-function csvwrite_with_headers(filename,m,headers,r,c)
+function csvwrite_with_headers(filename,data,headers,r,c)
 
 %% initial checks on the inputs
 if ~ischar(filename)
@@ -39,7 +39,7 @@ if ~iscellstr(headers)
 end
 
  
-if length(headers) ~= size(m,2)
+if length(headers) ~= size(data,2)
     error('number of header entries must match the number of columns in the data')
 end
 
@@ -59,6 +59,12 @@ if r>0
     end
 end
 
+%if the target folder doesn't exist, make it
+[output_folder,~,~] = fileparts(filename);
+if (not(exist(output_folder,'dir')))
+    mkdir(output_folder);
+end
+
 %write the string to a file
 fid = fopen(filename,'w');
 fprintf(fid,'%s\r\n',header_string);
@@ -69,4 +75,4 @@ fclose(fid);
 %
 % Call dlmwrite with a comma as the delimiter
 %
-dlmwrite(filename, m,'-append','delimiter',',','roffset', r,'coffset',c);
+dlmwrite(filename, data,'-append','delimiter',',','roffset', r,'coffset',c);
