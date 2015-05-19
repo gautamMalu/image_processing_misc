f82e588e6cb9c41b3b814e64f30cd5f4c1d9546d
Matthew Berginski <matthew.berginski@gmail.com>
2015-05-19 16:03:13 -0400

    function to find and fill holes only up to certain size

---
+++ fill_small_holes.m
@@ -0,0 +1,7 @@
+function filled_image = fill_small_holes(binary_image,max_hole_size)
+    filled_holes = imfill(binary_image,'holes') & not(binary_image);
+    
+    small_holes = bwpropclose(filled_holes,'Area',max_hole_size);
+    
+    filled_image = binary_image | filled_holes;
+end
