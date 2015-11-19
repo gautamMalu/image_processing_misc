e94e5e70a23a5a547a616728c51dfdb93825a514
Matthew Berginski <matthew.berginski@gmail.com>
2015-11-19 11:34:41 -0500

    adding option to filter with major/minor axis ratio

--- bwpropclose.m
+++ bwpropclose.m
@@ -18,7 +18,16 @@ i_p.parse(bw_image,prop_name,max_val,varargin{:});
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 bw_label = bwlabel(bw_image,i_p.Results.connectivity);
-props = regionprops(bw_label,prop_name);
+
+if (strcmp(i_p.Results.prop_name,'major_minor_ratio'))
+    props = regionprops(bw_label,'MajorAxisLength','MinorAxisLength');
+    ratio = [props.MajorAxisLength]./[props.MinorAxisLength];
+    for i=1:length(ratio)
+        props(i).('major_minor_ratio') = ratio(i);
+    end
+else
+    props = regionprops(bw_label,prop_name);
+end
 
 filtered_image = ismember(bw_label,find([props.(prop_name)] <= max_val));
 
