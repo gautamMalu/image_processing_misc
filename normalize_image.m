eac859dcdd7de5b291e114d125f279f75cbfb86f
Matthew Berginski <matthew.berginski@gmail.com>
2016-04-18 11:45:13 -0400

    adding option to only consider nonzero pixels for limits, output of selected limits

--- normalize_image.m
+++ normalize_image.m
@@ -1,4 +1,4 @@
-function image = normalize_image(image,varargin)
+function [image,varargout] = normalize_image(image,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Setup variables and parse command line
@@ -10,6 +10,7 @@ i_p.addRequired('image',@isnumeric);
 
 i_p.addParameter('quantiles',@isnumeric);
 i_p.addParameter('limits',@isnumeric);
+i_p.addParameter('only_nonzero',@islogical);
 
 i_p.addParameter('debug',0,@(x)(isnumeric(x) && (x == 0 || x == 1) || islogical(x)));
 
@@ -22,12 +23,18 @@ i_p.parse(image,varargin{:});
 image = double(image);
 
 if (not(any(strcmp(i_p.UsingDefaults,'quantiles'))))
-    limits = quantile(image(:),i_p.Results.quantiles);
+    if (not(any(strcmp(i_p.UsingDefaults,'only_nonzero'))))
+        limits = quantile(nonzeros(image(:)),i_p.Results.quantiles);
+    else
+        limits = quantile(image(:),i_p.Results.quantiles);
+    end
 elseif (not(any(strcmp(i_p.UsingDefaults,'limits'))))
     limits = i_p.Results.limits;
 else
     limits = [min(image(:)),max(image(:))];
 end
+
+varargout{1} = limits;
 
 image = (image - limits(1))/range(limits);
 
