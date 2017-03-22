c7670ee35d1dd963b22633e2e9c8773bab0450c7
Matthew Berginski <matthew.berginski@gmail.com>
2017-03-22 16:34:07 -0400

    adding option to label min/max value on map

--- output_color_map.m
+++ output_color_map.m
@@ -7,7 +7,8 @@ i_p = inputParser;
 
 i_p.addRequired('color_map',@(x)isnumeric(x));
 
-i_p.addParameter('width',10,@(x)isnumeric(x))
+i_p.addParameter('width',20,@(x)isnumeric(x));
+i_p.addParameter('labels',0,@(x)length(x) == 2);
 
 i_p.parse(color_map,varargin{:});
 
@@ -19,3 +20,17 @@ addpath(genpath('image_processing_misc'));
 
 [~,color_map_image] = meshgrid(1:i_p.Results.width,(size(color_map)-1):-1:0);
 color_map_image = ind2rgb(color_map_image,color_map);
+
+if (isempty(strcmp(i_p.UsingDefaults,'labels')))
+    color_map_image = insertText(color_map_image,[size(color_map_image,2)/2,1],...
+        i_p.Results.labels(1),'FontSize',8,'BoxOpacity',0,...
+        'AnchorPoint','CenterTop','TextColor','white');
+    
+    color_map_image = insertText(color_map_image,...
+        [size(color_map_image,2)/2,size(color_map_image,1)],...
+        i_p.Results.labels(2),'FontSize',8,'BoxOpacity',0,...
+        'AnchorPoint','CenterBottom','TextColor','white');
+end
+
+
+1;
