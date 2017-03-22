6cf684cfeeaa857b66a7a39fc05e00689adc454f
Matthew Berginski <matthew.berginski@gmail.com>
2017-03-22 12:43:05 -0400

    adding documentation, input parsing

--- apply_high_pass_filter.m
+++ apply_high_pass_filter.m
@@ -1,5 +1,20 @@
 function high_pass_image = apply_high_pass_filter(image,filter_size)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+i_p = inputParser;
+
+i_p.addRequired('image',@isnumeric);
+i_p.addRequired('filter_size',@(x)isnumeric(x) & x > 0);
+
+i_p.parse(image,filter_size);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 I_filt = fspecial('disk',filter_size);
 blurred_image = imfilter(image,I_filt,'same',mean(image(:)));
 high_pass_image = image - blurred_image;
