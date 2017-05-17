531a293643d1cd0006cb20c29b352fcc7b064b2a
HoffmanLab <bhoffmanlab@gmail.com>
2017-05-17 16:49:38 -0400

    adding documentation and parameter checking to function

--- imwrite_with_folder_creation.m
+++ imwrite_with_folder_creation.m
@@ -1,4 +1,23 @@
 function imwrite_with_folder_creation(image,file_target)
+%IMWRITE_WITH_FOLDER_CREATION    output a given image to target, with
+%creation of the folder
+%
+%   imwrite_with_folder_creation(I,FILE) outputs image, I, to file, FILE
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+i_p.FunctionName = 'IMWRITE_WITH_FOLDER_CREATION';
+
+i_p.addRequired('image',@(x)isnumeric(x) || islogical(x));
+i_p.addRequired('file_target',@(x)(ischar(x)));
+
+i_p.parse(image,file_target);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 [output_folder, ~, ~] = fileparts(file_target);
 mkdir_no_err(output_folder);
